9aa8ffad72dc3a8101eeef3d378bdaa08bbd190c
Riuno91 <riuno91@gmail.com>
2014-10-15 15:31:47 +0300

    kaosdkasdo

--- src/teine/kodutöö/algoritmid/implementations/PriorityQueue.java
+++ src/teine/kodutöö/algoritmid/implementations/PriorityQueue.java
@@ -1,5 +1,7 @@
 package teine.kodutöö.algoritmid.implementations;
 
+import com.sun.xml.internal.bind.v2.TODO;
+
 /**
  * Created by Riuno on 14.10.2014.
  */
@@ -10,14 +12,17 @@ public class PriorityQueue extends  DynamicArray{
      */
     public PriorityQueue() {
         // lisame esimese elemendi, et alates yhest hakataks t2itma
+        minBinHeap = new DynamicArray();
         minBinHeap.add(-11);
     }
 
     public void enqueue (int x){
+
         int positsion;  // int x-i positsiooni jaoks
         minBinHeap.add(x); // lisain xi alguses arraysse
 
-        positsion = minBinHeap.len() -1 ; // kuna x on viimane lisatu, v6tan ta postisiooni array viimase elemendi j2rgi
+        positsion = minBinHeap.len() - 1; //TODO: VIGAA!!!, võtab viimase positsiooni, loogikaviga
+        // kuna x on viimane lisatu, v6tan ta postisiooni array viimase elemendi j2rgi
         while(true){
             int upParentPos, parent;  // int-d k6rval oleva elemendi jaoks
 
@@ -32,16 +37,28 @@ public class PriorityQueue extends  DynamicArray{
             if(parent < x){//kui parent on v2iksem x-st(min kuhja reegel), siis l6petan
                 break;
             }else{ // vastasel korral vahetan positsioonid
+                System.out.println("Seis enne sisestamist");
+                kontroll();
                 minBinHeap.put(x, upParentPos);
+                System.out.println("Seis peale x koha muutmist sisestamist ");
+                kontroll();
                 minBinHeap.put(parent, positsion);
+                System.out.println("positsion = " + positsion);
+                System.out.println("Seis peale parenti koha muutmist sisestamist ");
+                kontroll();
             }
 
             positsion = upParentPos; // m22ran elemendi positsiooniks ylemelemendi positsiooni
         }
-
     }
 
     public int dequeue(){
+
+       /* for (int i = 1; i < minBinHeap.len()-1;i++){
+            System.out.print(minBinHeap.get(i) + " ");
+        }
+        System.out.println();*/
+
 
         int n = 0, last = 0; //int-d, yks viimane teine esimene element
 
@@ -64,4 +81,13 @@ public class PriorityQueue extends  DynamicArray{
         }else return false;
 
     }
+
+    void kontroll(){
+        for (int i = 1; i < minBinHeap.len()-1;i++){
+            System.out.print(minBinHeap.get(i) + " ");
+        }
+
+        System.out.println();
+    }
+
 }
